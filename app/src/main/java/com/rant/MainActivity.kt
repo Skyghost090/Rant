bd379a4d92a32a1dea9078dec0b562ecef4c0723
Skyghost090 <120269841+Skyghost090@users.noreply.github.com>
2025-03-23 21:44:28 -0300

    Fix layout height with keyboard opened

--- app/src/main/java/com/rant/MainActivity.kt
+++ app/src/main/java/com/rant/MainActivity.kt
@@ -31,7 +31,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            val imeInsets = insets.getInsets(WindowInsetsCompat.Type.ime())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom + (imeInsets.bottom * 1.0).roundToInt())
             insets
         }
         val floatButton_ = findViewById<ImageView>(R.id.floatingbutton)
